c2204e82678b2a5c65d12b01d4f972260465c6dc
0xF32 <192173331+0xF32@users.noreply.github.com>
2025-05-13 15:46:03 +1000

    wallpaper, styling and javascript variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+let windowMoveAmount = 100;
+let windowResizeAmount = 10;
+
 function init() {
   document.addEventListener("keydown", keydown, false);
   console.log("Init done!");
@@ -5,10 +8,12 @@ function init() {
 
 function makeMain(element) {
   console.log("making main: ", element);
-  document.querySelectorAll("#mainWindow").forEach((el) => {
-    el.id = "";
+  document.querySelectorAll("window").forEach((el) => {
+    el.id = "subWindow";
+    el.style.setProperty("--z-index", parseInt(getComputedStyle(el).getPropertyValue("--z-index")) - 1)
   });
   element.id = "mainWindow";
+  element.style.setProperty("--z-index", 3) // Set to the number of windows when adding a new window #TODO
 }
 
 function keydown(e) {
@@ -20,7 +25,7 @@ function keydown(e) {
     // Left arrow
     if (e.keyCode == 37) {
       let width = parseInt(mainWindowStyle.getPropertyValue("--width"));
-      width -= 10;
+      width -= windowResizeAmount;
       width += "px";
       mainWindow.style.setProperty("--width", width);
       console.log("Shift + left arrow down: width =", width);
@@ -28,7 +33,7 @@ function keydown(e) {
     // Up Arrow
     if (e.keyCode == 38) {
       let height = parseInt(mainWindowStyle.getPropertyValue("--height"));
-      height -= 10;
+      height -= windowResizeAmount;
       height += "px";
       mainWindow.style.setProperty("--height", height);
       console.log("Shift + up arrow down: height =", height);
@@ -36,7 +41,7 @@ function keydown(e) {
     // Right Arrow
     if (e.keyCode == 39) {
       let width = parseInt(mainWindowStyle.getPropertyValue("--width"));
-      width += 10;
+      width += windowResizeAmount;
       width += "px";
       mainWindow.style.setProperty("--width", width);
       console.log("Shift + right arrow down: width =", width);
@@ -44,7 +49,7 @@ function keydown(e) {
     // Down Arrow
     if (e.keyCode == 40) {
       let height = parseInt(mainWindowStyle.getPropertyValue("--height"));
-      height += 10;
+      height += windowResizeAmount;
       height += "px";
       mainWindow.style.setProperty("--height", height);
       console.log("Shift + down arrow down: height =", height);
@@ -54,7 +59,7 @@ function keydown(e) {
     // Left arrow
     if (e.keyCode == 37) {
       let x = parseInt(mainWindowStyle.getPropertyValue("--x"));
-      x -= 10;
+      x -= windowMoveAmount;
       x += "px";
       mainWindow.style.setProperty("--x", x);
       console.log("left arrow down: x =", x);
@@ -62,7 +67,7 @@ function keydown(e) {
     // Up Arrow
     if (e.keyCode == 38) {
       let y = parseInt(mainWindowStyle.getPropertyValue("--y"));
-      y -= 10;
+      y -= windowMoveAmount;
       y += "px";
       mainWindow.style.setProperty("--y", y);
       console.log("up arrow down: y =", y);
@@ -70,7 +75,7 @@ function keydown(e) {
     // Right Arrow
     if (e.keyCode == 39) {
       let x = parseInt(mainWindowStyle.getPropertyValue("--x"));
-      x += 10;
+      x += windowMoveAmount;
       x += "px";
       mainWindow.style.setProperty("--x", x);
       console.log("right arrow down: x =", x);
@@ -78,7 +83,7 @@ function keydown(e) {
     // Down Arrow
     if (e.keyCode == 40) {
       let y = parseInt(mainWindowStyle.getPropertyValue("--y"));
-      y += 10;
+      y += windowMoveAmount;
       y += "px";
       mainWindow.style.setProperty("--y", y);
       console.log("down arrow down: y =", y);
@@ -88,15 +93,28 @@ function keydown(e) {
 
 // Get the value of a global css variable
 // Nicknamed settings, so that it can be configured
-function getSetting(setting) {
+function getCSSSetting(setting) {
   let rootStyle = getComputedStyle(document.querySelector(":root"));
   let value = rootStyle.getPropertyValue(setting);
   return value;
 }
 
 // Set the value of a global css variable
-function setSetting(setting, value) {
+function setCSSSetting(setting, value) {
   let root = document.querySelector(":root");
   root.style.setProperty(setting, value);
-  console.log(setting, "=", getSetting(setting))
+  console.log(setting, "=", getSetting(setting));
+}
+
+
+// For JavaScript:
+// Get the value of a variable
+function getJSSetting(setting) {
+  return (1, eval)(setting);
+}
+
+// Set the value of a variable
+function setJSSetting(setting, value) {
+  (1, eval)(setting + '=' + value);
+  console.log(setting, "=", getJSSetting(setting));
 }
