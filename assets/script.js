c0ed009edf56919ea29ab22e7bdb79d343800bdb
0xF32 <192173331+0xF32@users.noreply.github.com>
2025-05-16 15:31:45 +1000

    tweaks and more commands

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let windowMoveAmount = 100;
+let windowMoveAmount = 10;
 let windowResizeAmount = 10;
 
 function init() {
@@ -20,6 +20,8 @@ function makeMain(element) {
 }
 
 function keydown(e) {
+  if (document.getElementById("terminal_input") == document.activeElement)
+    return;
   // Get the window and its style for easy access
   let mainWindow = document.getElementById("mainWindow");
   let mainWindowStyle = getComputedStyle(mainWindow);
@@ -122,46 +124,93 @@ function setJSSetting(setting, value) {
 }
 
 // Terminal implementation
-let prompt =
+const prompt =
   "~ ||> <input id='terminal_input' type='text' onchange='runCommand(this.value)'/>";
+let available_commands = ["help", "ls", "clear", "echo", "loop", "neofetch"];
 // Run Command
 function runCommand(value) {
+  // Handle enter
+  document.getElementById("terminal_input").remove();
+  document.getElementById("active_terminal").innerHTML =
+    document.getElementById("active_terminal").innerHTML + value + "<br />";
+  // Convert into command and args
   value = value.split(" ");
   let command = value[0];
   value.shift();
   let args = value.join(" ");
-  console.log("Running", command + "('" + args + "')");
-  (1, eval)(command + "('" + args + "')");
+  // Check if the command exists
+  if (available_commands.includes(command)) {
+    // Log
+    console.log("Running", command + "('" + args + "')");
+    // Run with eval
+    (1, eval)(command + "('" + args + "')");
+  } else {
+    // The command isn't available
+    document.getElementById("active_terminal").innerHTML =
+      document.getElementById("active_terminal").innerHTML +
+      "`" +
+      command +
+      "` is not available, use `help` to see a list of commands.<br />";
+  }
+  // Once the command is finished, show the prompt again
+  document.getElementById("active_terminal").innerHTML =
+    document.getElementById("active_terminal").innerHTML + prompt;
+  // Refocus input
   document.getElementById("terminal_input").focus();
 }
 
 // Make a function for each command implemented.
 
-// LS function (lists directories in current directory)
-function ls(args) {
-  document.getElementById("terminal_input").remove();
+// HELP function (lists all available commands)
+function help(_args) {
   document.getElementById("active_terminal").innerHTML =
     document.getElementById("active_terminal").innerHTML +
-    "ls " +
-    args +
-    "<br />" +
-    prompt;
+    available_commands.join("<br />") +
+    "<br />";
+}
+
+// LS function (lists directories in current directory)
+function ls(_args) {
+  document.getElementById("active_terminal").innerHTML =
+    document.getElementById("active_terminal").innerHTML;
 }
 
 // CLEAR function (removes all text in the terminal)
-function clear(args) {
-  document.getElementById("active_terminal").innerHTML = prompt;
+function clear(_args) {
+  document.getElementById("active_terminal").innerHTML = "";
 }
 
 // ECHO function (prints input text)
 function echo(args) {
-  document.getElementById("terminal_input").remove();
   document.getElementById("active_terminal").innerHTML =
-    document.getElementById("active_terminal").innerHTML +
-    "echo " +
-    args +
-    "<br /><br />" +
-    args +
-    "<br /><br />" +
-    prompt;
+    document.getElementById("active_terminal").innerHTML + args + "<br />";
+}
+
+// LOOP function (repeats the function contained for the specified number of times)
+function loop(input_args) {
+  input_args = input_args.split(" ");
+  let number = input_args[0];
+  input_args.shift();
+  // Convert into command and args
+  let command = input_args[0];
+  input_args.shift();
+  let args = input_args.join(" ");
+  // Run the command the specified number of times
+  for (let i = 0; i < number; i++) {
+    // Log
+    console.log("Running", command + "('" + args + "')");
+    // Run with eval
+    (1, eval)(command + "('" + args + "')");
+  }
+}
+
+// NEO FETCH function (fancy system info display and shows distro)
+function neofetch(_args) {
+  document.getElementById("active_terminal").innerHTML =
+    document.getElementById("active_terminal").innerHTML + "Your Browser: " + window.navigator.userAgent + "<br />";
+}
+
+// RESET TERMINAL function (debug to reset the terminal from the browser console)
+function reset_terminal() {
+  document.getElementById("active_terminal").innerHTML = prompt;
 }
